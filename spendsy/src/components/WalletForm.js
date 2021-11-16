b558baa12378494a5de5204356ab6de9631b832b
Aidan Margo <aidanmargo@gmail.com>
2021-11-16 15:51:33 -0600

    Stlyed Wallet Form

--- spendsy/src/components/WalletForm.js
+++ spendsy/src/components/WalletForm.js
@@ -1,21 +1,54 @@
+import NavBar from "./Navbar";
+import {useState} from 'react'
+
 function WalletForm() {
+
+  const [billList, setBillList] = useState([])
+  const [formData, setFormData] = useState({
+    bill_name: "",
+    bill_amount: 0,
+    category_name: "",
+  })
+
+  const addToBills= (e) => {
+    e.preventDefault()
+    setFormData({...formData, [e.target.name]:e.target.value});
+    console.log(formData)
+    };
+
+    console.log(formData)
   return (
     <div>
-      <form>
+      <NavBar />
+      <form className="bill-entry">
         <label>Bill:</label>
         <br />
-        <input type="text" name="bill" placeholder="Bill Name"></input>
+        <input type="text" name="bill" placeholder="Bill Name" className="bill-input-field" value={formData.bill_name} onChange={addToBills}>
+        </input>
         <br />
         <label>Cost:</label>
         <br />
-        <input type="text" name="cost" placeholder="Cost"></input>
+        <input type="number" name="cost" placeholder="Cost" className="bill-input-field" value={formData.bill_amount} onChange={addToBills}></input>
         <br />
         <label>Category:</label>
         <br />
-        <input type="text" name="category" placeholder="Category"></input>
+        <select className="bill-input-field" value={formData.category_name} onChange={addToBills}>
+          <option value="housing">Housing</option>
+          <option value="personals">Personals</option>
+          <option value="food">Food</option>
+          <option value="transportation">Transportation</option>
+          <option value="utilities">Utilities</option>
+          <option value="savings">Savings</option>
+          <option value="misc">Misc.</option>
+        </select>
         <br />
-        <button type="button">Add Bill</button>
+        <br />
+        <button type="button" className="btn" onSubmit={(e) => addToBills(e)}>Add Bill</button>
       </form>
+
+
+
+
     </div>
   );
 }
