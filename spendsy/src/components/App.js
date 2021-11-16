5082b23fff7f31ca709cc63c3ea9bef95be74225
Yeison <enmanuelquiterios@gmail.com>
2021-11-16 10:18:46 -0500

    Fixed UseEffect

--- spendsy/src/components/App.js
+++ spendsy/src/components/App.js
@@ -1,23 +1,22 @@
-import Landing from './Landing'
-import Wallet from './Wallet'
-import {useState, useEffect} from 'react'
+import Landing from "./Landing";
+import Wallet from "./Wallet";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [wallets, setWallets] = useState({});
 
-  const [wallets, setWallets] = useState({})
+  useEffect(() => {
+    fetch("http://localhost:9292/wallet")
+      .then((resp) => resp.json())
+      .then((data) => setWallets(data));
+  }, []);
 
-  useEffect(
-    fetch('http://localhost:9292/wallet')
-    .then(resp => resp.json())
-    .then(data => setWallets(data))
-  , [])
-
-  console.log(wallets)
+  console.log(wallets);
 
   return (
     <div className="App">
       <Landing />
-      <Wallet wallets={wallets}/>
+      <Wallet wallets={wallets} />
     </div>
   );
 }
